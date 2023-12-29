04a63533d71a80b561d7d6b16c076fbbf370d3b4
Raj Mahesuriya <raj.mahesuriya@groovyweb.co>
2023-12-29 18:27:50 +0530

    Fixed: navigation login issue

--- src/app/Pages/restaurant-items/restaurant-items.component.ts
+++ src/app/Pages/restaurant-items/restaurant-items.component.ts
@@ -1,14 +1,14 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { MasterService } from '../../services/master.service';
-import { CommonModule } from '@angular/common';
+import { Component } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { MasterService } from "../../services/master.service";
+import { CommonModule } from "@angular/common";
 
 @Component({
-  selector: 'app-restaurant-items',
+  selector: "app-restaurant-items",
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './restaurant-items.component.html',
-  styleUrl: './restaurant-items.component.css',
+  templateUrl: "./restaurant-items.component.html",
+  styleUrl: "./restaurant-items.component.css",
 })
 export class RestaurantItemsComponent {
   items: any[] = [];
@@ -31,15 +31,15 @@ export class RestaurantItemsComponent {
 
   navigate(item: string) {
     console.log(item);
-    const loggedData = localStorage.getItem('zomatoUser');
-    console.log('loggedData', loggedData);
+    const loggedData = localStorage.getItem("zomatoUser");
+    console.log("loggedData", loggedData);
 
     if (!!loggedData) {
-      console.log('loggedData');
-      this.router.navigate(['/restaurant-items', item]);
+      console.log("loggedData");
+      this.router.navigate(["/restaurant-items", item]);
     } else {
-      console.log('Not loggedData');
-      this.router.navigateByUrl('/login');
+      console.log("Not loggedData");
+      this.router.navigate(["/login"]);
     }
   }
 }
